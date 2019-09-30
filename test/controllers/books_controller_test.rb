48dc0705ec98db2a4a19100859bdcf65f5c1cc8d
Devin Helmgren <devincollege@gmail.com>
2019-09-30 14:37:51 -0700

    quick refactors

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 describe BooksController do
-  it "can render the index" do
-    get "/books"
-    must_respond_with :success
+
+  describe "Index" do
+    it "can render the index" do
+      get "/books"
+      must_respond_with :success
+    end
   end
+  
 end
