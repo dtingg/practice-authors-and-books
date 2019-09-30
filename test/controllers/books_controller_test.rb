81a89de8d59080f0a4fbd37b8b07bab59d9db9cd
Devin Helmgren <devincollege@gmail.com>
2019-09-30 14:49:59 -0700

    Added some beautiful tests for the show action

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -8,5 +8,17 @@ describe BooksController do
       must_respond_with :success
     end
   end
-  
+
+  describe "Show" do
+    it "can render an extant book" do
+      get "/books/1"
+      must_respond_with :success
+    end
+
+    it "responds 'not found' for an non-existant book" do
+      get "/books/5000000"
+      must_respond_with :error
+    end
+  end
+
 end
