426655f680de5231e08ad2cc323685e6ff526a81
beccaelenzil <beccaelenzil@gmail.com>
2019-10-02 14:28:59 -0700

    form scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     # Routes that operate on the book collection
+    root 'books#index'
     get '/books', to: 'books#index', as: 'books'
     get '/books/new', to: 'books#new', as: 'new_book'
     post '/books', to: 'books#create'
