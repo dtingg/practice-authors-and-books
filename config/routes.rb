497fb1d93ee56d2eebbf4e50e2c66bb5625b2fe4
beccaelenzil <beccaelenzil@gmail.com>
2019-10-02 13:55:21 -0700

    view helpers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # verb 'path', to: 'controller#action'
-  get '/books', to: 'books#index'
-  get '/books/:id', to: 'books#show'
+  get '/books', to: 'books#index', as: 'books'
+  get '/books/:id', to: 'books#show', as: 'book'
   
 end
