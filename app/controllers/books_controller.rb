6d4d696f34aac2ddd2074b49e88856cc6a03b9c2
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-10-04 13:18:58 -0700

    Style our form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,21 +2,21 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
-
+  
   def show
     book_id = params[:id]
     @book = Book.find_by(id: book_id)
-
+    
     if @book.nil?
       head :not_found
       return
     end
   end
-
+  
   def new
     @book = Book.new
   end
-
+  
   def create
     @book = Book.new(book_params) #instantiate a new book
     if @book.save # save returns true if the database insert succeeds
@@ -27,16 +27,16 @@ class BooksController < ApplicationController
       return
     end
   end
-
+  
   def edit
     @book = Book.find_by(id: params[:id])
-
+    
     if @book.nil?
       head :not_found
       return
     end
   end
-
+  
   def update
     @book = Book.find_by(id: params[:id])
     if @book.update(book_params)
@@ -47,27 +47,27 @@ class BooksController < ApplicationController
       return
     end
   end
-
+  
   def destroy
     book_id = params[:id]
     @book = Book.find_by(id: book_id)
-
+    
     if @book.nil?
       head :not_found
       return
     end
-
+    
     @book.destroy
-
+    
     redirect_to books_path
     return
   end
-
+  
   private
-
+  
   def book_params
     return params.require(:book).permit(:author, :title, 
-                                         :description)
+    :description)
   end
-
+  
 end
