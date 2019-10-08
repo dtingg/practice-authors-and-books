da19d769c37bde1aacce4506941ee3c36110c581
Dianna Tingg <dtingg@gmail.com>
2019-10-08 10:02:59 -0700

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def show
+    @author = Author.find_by(id: params[:id])
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :author
+  validates :title, presence: true
+  
+  def self.alpha_books
+    return Book.order(title: :asc)
+  end
 end
